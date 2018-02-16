6d07ad12e67af11518f28ca970af2eaeda35e0f3
Sebastien Duthil <sduthil@wazo.community>
2018-02-16 13:43:15 -0500

    add -a and -m flags for explicit repos
    reason: we will add more distributions like wazo-dev-buster. The current
    whitelist-based repo-choosing strategy does not scale very well with the
    new distributions. Also, giving the repo flags is more explicit.

--- xivo_dist/xivo_dist.py
+++ xivo_dist/xivo_dist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright 2014-2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2014-2018 The Wazo Authors  (see the AUTHORS file)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -40,35 +40,47 @@ deb http://{host}{path} {distrib} main
 
 def main():
     args = parse_args()
-    distribution_sources = generate_sources(args.distribution, args.host, args.path)
+    path = determine_path(args.distribution, args.path, args.archive_repo, args.main_repo)
+    distribution_sources = generate_sources(args.distribution, args.host, path)
     write_source_list(distribution_sources)
 
 
 def parse_args():
     parser = argparse.ArgumentParser('xivo-dist')
     parser.add_argument('--host', default=DEFAULT_HOST)
+
+    group = parser.add_mutually_exclusive_group()
     parser.add_argument('--path')
-    parser.add_argument('distribution', help='switch sources to given distribution')
+    group.add_argument('-a', '--archive-repo', action='store_true', help='Use archive repository')
+    group.add_argument('-m', '--main-repo', action='store_true', help='Use main repository')
+
+    parser.add_argument('distribution', help='Switch sources to given distribution')
 
     return parser.parse_args()
 
 
 def write_source_list(distribution_sources):
-    fh = open(SOURCE_LIST_PATH, 'w')
-    fh.write(distribution_sources)
-    fh.close()
+    with open(SOURCE_LIST_PATH, 'w') as source_file:
+        source_file.write(distribution_sources)
 
 
 def generate_sources(distribution, host, path):
-    if path is None:
-        if distribution_is_archive(distribution):
-            path = '/archive/'
-        else:
-            path = '/debian/'
-
     deb_source = DEB_SOURCE_CONTENT.format(host=host, path=path, distrib=distribution)
 
     return deb_source
+
+
+def determine_path(distribution, custom_path, archive_repo, main_repo):
+    if main_repo:
+        return '/debian/'
+    if archive_repo:
+        return '/archive/'
+    if custom_path:
+        return custom_path
+    if distribution_is_archive(distribution):
+        return '/archive/'
+
+    return '/debian/'
 
 
 def distribution_is_archive(distribution):
