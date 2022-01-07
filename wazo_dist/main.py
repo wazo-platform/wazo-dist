0603ee502bcb2403ff60b0d6ec68e126c4c78448
Francois Blackburn <fblackburn@wazo.community>
2022-01-07 09:01:35 -0500

    remove unnecessary shebang on main file

--- wazo_dist/main.py
+++ wazo_dist/main.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# Copyright 2014-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2014-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import argparse
